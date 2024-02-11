8efccfbb6bcff87d3320f489c353f8559cbae907
Rakhi Mandal <rakhimandal2022@gmail.com>
2024-02-11 12:22:09 +0530

    commit done

---
+++ SkillGapUI/angular-workspace/projects/skill-gap-analysis/src/app/employee-skills/employee-skills.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-skills',
+  templateUrl: './employee-skills.component.html',
+  styleUrl: './employee-skills.component.css'
+})
+export class EmployeeSkillsComponent {
+
+}
---
+++ SkillGapUI/angular-workspace/projects/skill-gap-analysis/src/app/employee-trainings/employee-trainings.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-trainings',
+  templateUrl: './employee-trainings.component.html',
+  styleUrl: './employee-trainings.component.css'
+})
+export class EmployeeTrainingsComponent {
+
+}
